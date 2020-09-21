1d9f4e964d17fd9b0f9a7557a832e995fd9a479c
Eike Spielberg <Eike.Spielberg@uni-due.de>
2020-09-21 11:47:50 +0200

    adding call waiting time to avoid API overload
    adding liable for vat, cleaning up addresses, logging

--- clean_up_after_migration.py
+++ clean_up_after_migration.py
@@ -1,6 +1,7 @@
 import json
 import logging
 import os
+import time
 
 import requests
 
@@ -174,6 +175,69 @@ def deactivate_non_used_vendors(type):
             logging.error(get_list.text)
 
 
+def set_liable_for_vat():
+    # Datei mit den Lieferanten mit suffix finance laden
+    vendors = load_identifier_list_of_type('vendors_finance')
+
+    # API-Key aus den Umgebungsvariablen lesen
+    api_key = os.environ['ALMA_SCRIPT_API_KEY']
+
+    # alle Lieferanten durchgehen
+    for vendor_line in vendors:
+
+        time.sleep(0.05)
+
+        # Die URL für die API zusammensetzen
+        url = '{}acq/vendors/{}?apikey={}'.format(alma_api_base_url, vendor_line, api_key)
+
+        # GET-Abfrage ausführen
+        get = requests.get(url=url, headers={'Accept': 'application/json'})
+
+        # Kodierung auf UTF-8 festsetzen
+        get.encoding = 'utf-8'
+
+        # Prüfen, ob die Abfrage erfolgreich war (Status-Code ist dann 200)
+        if get.status_code == 200:
+
+            # Den Inhalt der Antwort als Text auslesen
+            info = get.text
+
+            # In dem Text den Typ auf Inactive ändern
+            payload = info.replace('"liable_for_vat":false,', '"liable_for_vat":true,')
+
+            if '"country":{"value":"DE"' in payload:
+                payload = payload.replace('"country":{"value":"DE","desc":""}', '"country":{"value":"DEU","desc":"Germany"}')
+
+            if '"country":{"value":"Germany"' in payload:
+                payload = payload.replace('"country":{"value":"Germany","desc":""}', '"country":{"value":"DEU","desc":"Germany"}')
+
+            if '"country":{"value":"Deutschland"' in payload:
+                payload = payload.replace('"country":{"value":"Deutschland","desc":""}', '"country":{"value":"DEU","desc":"Germany"}')
+
+            if '"country":{"value":"Schweiz"' in payload:
+                payload = payload.replace('"country":{"value":"Schweiz","desc":""}', '"country":{"value":"CHE","desc":"Germany"}')
+
+            if '"country":{"value":"Japan"' in payload:
+                payload = payload.replace('"country":{"value":"Japan","desc":""}', '"country":{"value":"JPN","desc":"Germany"}')
+
+            if '"country":{"value":"Belgien"' in payload:
+                payload = payload.replace('"country":{"value":"Belgien","desc":""}', '"country":{"value":"BEL","desc":"Germany"}')
+
+            # Update als PUT-Abfrage ausführen. URL ist die gleiche, Encoding ist wieder utf-8, Inhalt ist JSON
+            update = requests.put(url=url, data=payload.encode('utf-8'), headers={'Content-Type': 'application/json'})
+
+            # Antwort-Encoding ist wieder UTF-8
+            update.encoding = 'utf-8'
+
+            # Prüfen, ob Anfrage erfolgreich war und alles in die Log-Datei schreiben
+            if update.status_code == 200:
+                logging.info('succesfully updated liable for vat for vendor {}'.format(vendor_line))
+            else:
+                logging.warning('problem updating liable for vat for vendor {}:{}'.format(vendor_line, update.text))
+        else:
+            logging.error(get.text)
+
+
 def fill_financial_code():
     # Datei mit den Lieferanten mit suffix finance laden
     vendors = load_identifier_list_of_type('vendors_finance')
@@ -183,6 +247,8 @@ def fill_financial_code():
 
     # alle Lieferanten durchgehen
     for vendor_line in vendors:
+
+        time.sleep(0.05)
 
         # Die URL für die API zusammensetzen
         url = '{}acq/vendors/{}?apikey={}'.format(alma_api_base_url, vendor_line, api_key)
@@ -199,11 +265,29 @@ def fill_financial_code():
             # Den Inhalt der Antwort als JSON-Objekt auslesen
             get_json = get.json()
 
-            # den SAP-Kreditoren-Wert aus dem Feld 'additional_code' auslesen
-            financial_code = get_json['additional_code']
-
+            try:
+                if get_json['financial_sys_code'] != '':
+                    logging.info('financial code already set for vendor {}'.format(vendor_line))
+                    # den SAP-Kreditoren-Wert aus dem Feld 'additional_code' auslesen
+            except KeyError:
+                logging.debug('no financial code set for vendor {}'.format(vendor_line))
+            try:
+                financial_code = get_json['additional_code']
+            except KeyError:
+                logging.error('no additional code in vendor {}'.format(vendor_line))
+                continue
             # den ausgelesenen Wert in das Feld 'financial_sys_code' schreiben
             get_json['financial_sys_code'] = financial_code
+
+            # get_json['additional_code'] = ""
+
+            for index, currency in enumerate(get_json['currency']):
+                if currency['value'] == 'DEM':
+                    get_json['currency'].pop(index)
+
+            for address in get_json['contact_info']['address']:
+                if address['country']['value'] == 'Deutschland':
+                     address['country']['value'] = 'DEU'
 
             # den aktualisierten Lieferanten per PUT-Abfrage nach Alma schreiben (gleiche url wie bei der Abfrage,
             # das JSON-object wird über json.dumps in einen Text umgewandelt.
@@ -212,11 +296,12 @@ def fill_financial_code():
             # die Kodierung der Antwort setzen
             update.encoding = 'utf-8'
 
-            # Prüfen, ob die Abfrage erfolgreich war (Status Code 200) und alles in die Log-Datei schreiben
+             # Prüfen, ob die Abfrage erfolgreich war (Status Code 200) und alles in die Log-Datei schreiben
             if update.status_code == 200:
                 logging.info('succesfully updated vendor {}'.format(vendor_line))
             else:
                 logging.error('problem updating vendor {}:{}'.format(vendor_line, update.text))
+
         else:
             logging.error(get.text)
 
@@ -231,7 +316,7 @@ if __name__ == '__main__':
     log_file = 'data/output/{}.log'.format(run_name)
 
     # den Logger konfigurieren (Dateinamen, Level)
-    logging.basicConfig(filename=log_file, level=logging.DEBUG)
+    logging.basicConfig(format='%(asctime)s %(levelname)-8s %(message)s', filename=log_file, level=logging.INFO)
 
     # welcher Lauf gestartet werden soll. Zum Aufrufen einer Funktion den entsprechenden Aufruf durch Entfernen des '#'
     # aktivieren
@@ -240,6 +325,6 @@ if __name__ == '__main__':
     # update_vendors('do')
     # deactivate_non_used_vendors(run_name)
     # check_log(run_name)
-    fill_financial_code()
-
+    # fill_financial_code()
+    set_liable_for_vat()
 
