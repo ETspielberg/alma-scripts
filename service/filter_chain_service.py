c8722e20892264b2bb511d68d02b46728a230cbc
Eike Spielberg <Eike.Spielberg@uni-due.de>
2020-11-02 08:11:31 +0100

    rmoving update_users.py, now part of clean_up.

--- service/filter_chain_service.py
+++ service/filter_chain_service.py
@@ -11,10 +11,17 @@ def load_line_checker_list(project):
         chain_json = json.load(json_file)
         json_file.close()
         file = chain_json['file']
-        list_filter = ListFilter(project=project, filename=file)
+        record_type = chain_json['record_type']
+        list_filter = ListFilter(project=project, filename=file, record_type=record_type)
+        try:
+            format = chain_json['format']
+        except KeyError:
+            format = ''
         for line_checker in chain_json['lineCheckers']:
+
             list_filter.add_line_checker(LineChecker(method_name=line_checker['method_name'],
                                                      checklist=line_checker['checklist'],
                                                      field=line_checker['field'], position=line_checker['position'],
-                                                     mode=line_checker['mode']))
+                                                     mode=line_checker['mode'],
+                                                     format=format))
     return list_filter
