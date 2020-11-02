3675d648906609de4aa4e9fb03d70ee0e9aeae1b
Eike Spielberg <Eike.Spielberg@uni-due.de>
2020-11-02 08:11:55 +0100

    added utf-8 encoding

--- service/list_reader_service.py
+++ service/list_reader_service.py
@@ -3,7 +3,7 @@ import os
 
 def load_identifier_list(provider):
     path_to_file = os.getcwd() + '/data/input/' + provider + '/isbn_list.txt'
-    with open(path_to_file) as f:
+    with open(path_to_file, encoding='utf-8') as f:
         isbns = f.readlines()
         f.close()
         # remove whitespace characters like `\n` at the end of each line
@@ -14,7 +14,7 @@ def save_identifier_list(provider, isbns):
     path_to_file = os.getcwd() + 'data/output/' + provider + '/isbn_list.txt'
     if not os.path.exists(path_to_file):
         os.makedirs(path_to_file)
-    with open(path_to_file, 'w') as list_file:
+    with open(path_to_file, 'w', encoding='utf-8') as list_file:
         for isbn in isbns:
             list_file.write(isbn + '\n')
         list_file.close()
@@ -25,7 +25,7 @@ def save_identifier_list_of_type(provider, identifier, list_type):
     if not os.path.exists(path_folder):
         os.makedirs(path_folder)
     path_to_file = path_folder + '/{}_list.txt'.format(list_type)
-    with open(path_to_file, 'w') as list_file:
+    with open(path_to_file, 'w', encoding='utf-8') as list_file:
         for isbn in identifier:
             list_file.write(isbn + '\n')
         list_file.close()
@@ -33,7 +33,7 @@ def save_identifier_list_of_type(provider, identifier, list_type):
 
 def load_identifier_list_of_type(identifier_type):
     path_to_file = os.getcwd() + '/data/input/' + identifier_type + '_list.txt'
-    with open(path_to_file) as f:
+    with open(path_to_file, encoding='utf-8') as f:
         isbns = f.readlines()
         f.close()
         # remove whitespace characters like `\n` at the end of each line
@@ -41,7 +41,7 @@ def load_identifier_list_of_type(identifier_type):
 
 
 def load_ids_from_file(path_to_file):
-    with open(path_to_file) as f:
+    with open(path_to_file, encoding='utf-8') as f:
         ids = f.readlines()
         f.close()
         # remove whitespace characters like `\n` at the end of each line
