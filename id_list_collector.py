eddd1d755253ccd307fa4df200ac47020cde2198
Eike Spielberg <Eike.Spielberg@uni-due.de>
2020-11-02 08:46:04 +0100

    changed column names in new Export file

--- id_list_collector.py
+++ id_list_collector.py
@@ -6,18 +6,18 @@ import requests
 
 def load_data(filename, library):
     temp_path = 'data/temp/ids_{}.txt'
-    table = pd.read_excel(filename, dtype={'String for Activationform': object, 'Inventory UB Duisburg-Essen': object, 'URL zur ID-Liste': object})
+    table = pd.read_excel('data/input/' + filename, dtype={'String for Activationform': object, 'Inventory UB Duisburg-Essen': object, 'klickbare URLs': object})
     for index, row in table.iterrows():
         try:
             if library in row['String for Activationform']:
-                r = requests.get(row['URL zur ID-Liste'])
+                r = requests.get(row['klickbare URLs'])
                 open(temp_path.format(index), 'wb').write(r.content)
         except:
-            print('no url in table field given')
+            print('no url in table field given for collection')
 
 
 def generate_output():
-    with open('data/output/all_records.xml', "w") as output_file:
+    with open('data/output/IZEXCLUDE_list.txt', "w") as output_file:
         for file in os.listdir('data/temp/'):
             if file.startswith('ids_'):
                 with open('data/temp/{}'.format(file), 'r') as input_file:
@@ -33,7 +33,7 @@ def generate_folders():
 
 if __name__ == '__main__':
     generate_folders()
-    filename = 'Verwaltung-NZ Liste der Kollektionen mit Downloadlinks.xlsx'
+    filename = 'Collections_NZ.xlsx'
     library = 'UB_DuE'
     load_data(filename, library)
     generate_output()
